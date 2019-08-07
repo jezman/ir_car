501f3932fe669a09fe2ce4ff2e1107462062d344
jezman <dev.jezman@gmail.com>
2019-08-07 08:55:12 +0300

    Fix debug define.

--- include/main.h
+++ include/main.h
@@ -2,4 +2,12 @@
 #include <irrecv.h>
 #include <l298n.h>
 
+#define VERBOSE
+
+#ifdef VERBOSE
+#define VERBOSE_PRINT(str) Serial.println(str)
+#else
+#define VERBOSE_PRINT(str)
+#endif
+
 void respondToIRRemote();
